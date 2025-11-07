324ff4d3c07d5f77cf881cb98dfcc5705caecb62
mariomuja <mario.muja@gmx.de>
2025-11-07 14:07:50 +0100

    Add TranslateModule back with proper factory configuration

--- bookkeeping-frontend/src/app/app.config.ts
+++ bookkeeping-frontend/src/app/app.config.ts
@@ -1,18 +1,34 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { BootstrapConfig, BOOTSTRAP_CONFIG, DocumentationConfig, DOCUMENTATION_CONFIG } from '@shared-components/services';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './services/auth.interceptor';
 import { environment } from '../environments/environment';
 
+// Factory function for TranslateLoader
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
     provideAnimations(),
+    importProvidersFrom(TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      },
+      defaultLanguage: 'en'
+    })),
     {
       provide: BOOTSTRAP_CONFIG,
       useValue: {
