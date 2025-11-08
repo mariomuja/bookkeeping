6b502a16a8929b6a139c03245ca5484ac86c1022
mariomuja <mario.muja@gmx.de>
2025-11-08 02:48:38 +0100

    feat: Switch to npm package @mario-muja/angular-shared-components instead of copied files

--- bookkeeping-frontend/src/app/components/documentation-modal/documentation-modal.component.ts
+++ bookkeeping-frontend/src/app/components/documentation-modal/documentation-modal.component.ts
@@ -1,5 +1,5 @@
 import { Component, Output, EventEmitter } from '@angular/core';
-import { SharedDocumentationModalComponent } from '../../shared/documentation';
+import { SharedDocumentationModalComponent } from '@mario-muja/angular-shared-components';
 
 @Component({
   selector: 'app-documentation-modal',
--- bookkeeping-frontend/src/app/shared/documentation/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './documentation-modal.component';
-
-
