4c7fc541dde411d3aabcb034cf032794794ff057
mariomuja <mario.muja@gmx.de>
2025-11-08 12:23:46 +0100

    Fix: Add TranslateModule configuration to enable sidebar translations

--- bookkeeping-frontend/src/app/app.config.ts
+++ bookkeeping-frontend/src/app/app.config.ts
@@ -1,18 +1,33 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, HttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { BootstrapConfig, BOOTSTRAP_CONFIG, DocumentationConfig, DOCUMENTATION_CONFIG } from '@mario-muja/angular-shared-components';
+import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './services/auth.interceptor';
 import { environment } from '../environments/environment';
+
+export function createTranslateLoader() {
+  return new TranslateHttpLoader();
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
     provideAnimations(),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        defaultLanguage: 'en',
+        loader: {
+          provide: TranslateLoader,
+          useFactory: createTranslateLoader
+        }
+      })
+    ),
     {
       provide: BOOTSTRAP_CONFIG,
       useValue: {
