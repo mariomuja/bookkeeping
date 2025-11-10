4baf0fdad3c3f33819b11f45982213bd2b4b9f5e
mariomuja <mario.muja@gmx.de>
2025-11-10 13:17:30 +0100

    Fix @ symbol in Impressum email for Angular template

--- bookkeeping-frontend/src/app/components/impressum/impressum.component.ts
+++ bookkeeping-frontend/src/app/components/impressum/impressum.component.ts
@@ -47,7 +47,7 @@ import { TranslateModule } from '@ngx-translate/core';
           <p>
             <strong>Telefon Deutschland:</strong> <a href="tel:+4915204641473">+49 1520 464 1473</a><br>
             <strong>Telefon Italien:</strong> <a href="tel:+393453450098">+39 345 345 0098</a><br>
-            <strong>E-Mail:</strong> <a href="mailto:mario.muja@gmail.com">mario.muja@gmail.com</a>
+            <strong>E-Mail:</strong> <a href="mailto:mario.muja&#64;gmail.com">mario.muja&#64;gmail.com</a>
           </p>
 
           <h3>Verantwortlich für den Inhalt nach § 55 Abs. 2 RStV</h3>
